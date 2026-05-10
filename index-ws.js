11fed20003e39635ae9ce489634167ea1304a3a4
Jen Chartier <JChartier001@gmail.com>
2026-05-10 13:09:25 -0400

    Created a websocket server and client

---
+++ index-ws.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const server = require('http').createServer();
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.sendFile('index.html', { root: __dirname });
+});
+
+server.on('request', app);
+
+server.listen(3000, () => {
+	console.log('Server started on port 3000');
+});
+
+/** Begin websocket */
+const WebSocketServer = require('ws').Server;
+
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', function connection(ws) {
+	const numClients = wss.clients.size;
+	console.log(`Number of connected clients: ${numClients}`);
+
+	wss.broadcast(`Current visitors: ${numClients}`);
+
+	if (ws.readyState === ws.OPEN) {
+		ws.send('Welcome to the WebSocket server!');
+	}
+	ws.on('close', function close() {
+		console.log('Client disconnected');
+	});
+});
+wss.broadcast = function broadcast(data) {
+	wss.clients.forEach(function each(client) {
+		client.send(data);
+	});
+};
